5208870b7577efa3281fb22e60f05e2b9030a709
Robson-tech <robson.junior@ufpi.edu.br>
2023-08-03 10:11:58 -0300

    Create carga.py

---
+++ carga.py
@@ -0,0 +1,9 @@
+class Carga:
+    def __init__(self, carga: int):
+        self.carga = carga
+
+    def get_carga(self):
+        return self.carga
+
+    def set_carga(self, carga):
+        self.carga = carga
