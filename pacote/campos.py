5a8db4fed05b22c97337d4815e91644c3090e00a
Robson-tech <robson.junior@ufpi.edu.br>
2023-08-04 23:38:50 -0300

    gitignore

--- pacote/campos.py
+++ pacote/campos.py
@@ -1,3 +1,7 @@
+from conf import *
+
+
 class Campo:
-    def __init__(self):
-        pass
+    def __init__(self, posicao: tuple, sinal_carga: int):
+        self.posicao = posicao
+        self.sinal_carga = sinal_carga
