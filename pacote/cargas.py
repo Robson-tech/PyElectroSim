86bc62bad20b882710207b3ec645c7e7287d2f68
Robson-tech <robson.junior@ufpi.edu.br>
2023-08-20 03:33:20 -0300

    multiplas cargas teste

--- pacote/cargas.py
+++ pacote/cargas.py
@@ -6,6 +6,7 @@ import pygame as py
 class Carga:
     def __init__(self, x: int, y: int, raio: int, num_eletrons: int, identificador: str = None):
         super().__init__()
+        self.rect = py.Rect(x, y, raio * 2, raio * 2)
         self.x_inicial = x
         self.y_inicial = y
         self.x = self.x_inicial
@@ -44,7 +45,7 @@ class Carga:
         dx = carga.x - self.x
         dy = carga.y - self.y
         quadrado_distancia = dx ** 2 + dy ** 2
-        if quadrado_distancia > (self.raio * 2) ** 2:
+        if quadrado_distancia:
             forca_eletrica = k * self.sinal * carga.sinal * -1 / quadrado_distancia
             angulo = math.atan2(dy, dx)
             forca_x += forca_eletrica * math.cos(angulo)
@@ -56,10 +57,12 @@ class Carga:
         for carga in cargas:
             if carga != self:
                 forca_x, forca_y = self.calcular_forca_eletrica(carga)
-                forca_total_x += forca_x
-                forca_total_y += forca_y
-        self.x += forca_total_x / self.massa * ESCALA
-        self.y += forca_total_y / self.massa * ESCALA
+                # forca_total_x += forca_x
+                # forca_total_y += forca_y
+                self.x += forca_x / self.massa * ESCALA
+                self.y += forca_y / self.massa * ESCALA
+        # self.x += forca_total_x / self.massa * ESCALA
+        # self.y += forca_total_y / self.massa * ESCALA
 
     def resetar(self):
         self.x = self.x_inicial
